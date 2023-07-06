28e1724082f6c08f61f23e3d5b882a161c440cd3
Christian Fritz <christian.fritz@gmail.com>
2023-07-05 17:28:31 -0700

    remove meow

--- source/cli.js
+++ source/cli.js
@@ -1,27 +1,9 @@
 #!/usr/bin/env node
 import React from 'react';
 import {render} from 'ink';
-import meow from 'meow';
 import App from './app.js';
 import fs from 'fs';
 import tty from 'tty';
-
-const cli = meow(
-	`
-		Usage
-		  $ node-line-select
-
-		Options
-			--name  Your name
-
-		Examples
-		  $ node-line-select --name=Jane
-		  Hello, Jane
-	`,
-	{
-		importMeta: import.meta,
-	},
-);
 
 const data = fs.readFileSync(process.stdin.fd, 'utf-8');
 
